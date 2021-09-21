d4ed7c95f7fafc670d3b7a93264de1742fd49278
kolluvinay425 <kolluvinay425@gmail.com>
2021-09-21 15:53:01 +0200

    added node version

--- src/server.js
+++ src/server.js
@@ -11,8 +11,19 @@ import {
 } from "./services/errorHandlers.js";
 //--------------------------GLOBAL MIDDLEWARES---------------------------------
 const server = express();
+const whitelist = [process.env.FE_DEV_URL, process.env.FE_PROD_URL];
+const corsOptions = {
+  origin: function (origin, next) {
+    console.log(origin);
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      next(null, true);
+    } else {
+      next(new Error(`origin ${origin} not allowed`));
+    }
+  },
+};
 // const port = process.env.PORT_NUM || 3001;
-server.use(cors()); //should be here to able to communicate Front-End with back-end
+server.use(cors(corsOptions)); //should be here to able to communicate Front-End with back-end
 server.use(express.json()); // if i do not specify this line before routes,all the request bodies will be undefined
 
 server.use("/authors", authorsRouter);
